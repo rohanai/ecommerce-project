b043ec96069f227ee100c4b179ecc0b84b7056c5
NikitaShah <nspinkyshah@gmail.com>
2020-01-09 23:24:38 +0530

    Redux-commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import './index.scss';
 import App from './App';
+import rootReducer from './reducers/rootReducer';
 
-const app = (
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-);
+const store = createStore(rootReducer);
 
-ReactDOM.render(app, document.getElementById('root'));
+// const app = (
+//     <BrowserRouter>
+//         <App />
+//     </BrowserRouter>
+// );
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
